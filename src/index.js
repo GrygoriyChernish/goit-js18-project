899fa85e4a800a44a919748841a314e34e29b97e
Хворостяный Денис Николаевич <d-h11@ukr.net>
2020-05-20 16:47:03 +0300

    fix(current-time.js): repack function

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
 import './scss/styles.scss';
+// import currentDate from './js/current-date';
+// console.log(currentDate);
 
+import moment from 'moment';
+
+const currentDay = moment().format('dddd').slice(0, -6);
+// const currentTime = moment().format('LTS');
+// console.log(moment().format('LL').split(' ')[0]);
+
+// const time = document.querySelector('#time');
+// const currentMounth = document.querySelector('#mounth');
+// const currentTime = moment().format('LTS');
+
+// const showTime = () => {
+//     time.innerHTML = `${currentTime}`;
+// };
+
+// showTime();
 const time = document.querySelector('#time');
 const currentMounth = document.querySelector('#mounth');
 
 const showTime = () => {
-    let today = new Date();
-    let hour = today.getHours();
-    let min = today.getMinutes();
-    let sec = today.getSeconds();
-
-    hour = hour % 24 || 24;
-
-    time.innerHTML = `${addZero(hour)}<span>:</span>${addZero(
-        min,
-    )}<span>:</span>${addZero(sec)}`;
-
+    time.innerHTML = `${moment().format('LTS')}`;
     setTimeout(showTime, 1000);
 };
 
-const addZero = number => (parseInt(number, 10) < 10 ? '0' : '') + number;
-
 showTime();
+
+const addZero = number => (parseInt(number, 10) < 10 ? '0' : '') + number;
 
 const showMounth = () => {
     let mounth = new Date();
---
+++ src/js/current-date.js
@@ -0,0 +1,5 @@
+import moment from 'moment';
+
+const currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+export default currentDate;
