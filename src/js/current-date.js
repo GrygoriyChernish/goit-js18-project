371fdaf82ee393c0ba226544b9db7a2dc2b10788
Denis <d-h11@ukr.net>
2020-05-20 23:18:49 +0300

    feat(current-date.js): fix code & add data in index.html

--- src/js/current-date.js
+++ src/js/current-date.js
@@ -1,5 +1,25 @@
 import moment from 'moment';
 
-const currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+const refs = {
+    date: document.querySelector('#data'),
+    suffix: document.querySelector('#suffix'),
+    dayWeek: document.querySelector('#day-week'),
+    time: document.querySelector('#time'),
+    mounth: document.querySelector('#mounth'),
+};
 
-export default currentDate;
+showTime();
+showCurrentData(refs.date, moment().format('Do').slice(0, 2));
+showCurrentData(refs.suffix, moment().format('Do').slice(2));
+showCurrentData(refs.dayWeek, moment().format('dddd').slice(0, -6));
+showCurrentData(refs.mounth, moment().format('LL').split(' ')[0]);
+
+function showCurrentData(link, value) {
+    link.innerHTML = `${value}`;
+}
+
+function showTime() {
+    time.innerHTML = `${moment().format('LTS')}`;
+
+    setInterval(showTime, 1000);
+}
