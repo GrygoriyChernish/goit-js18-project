4438ee27709e6a2712ace4ad5ca40bd512537469
Хворостяный Денис Николаевич <d-h11@ukr.net>
2020-05-21 16:31:39 +0300

    feat(templates for five days): create tmpl

--- src/index.js
+++ src/index.js
@@ -5,3 +5,22 @@ import showCurrentData from './js/show-current-data';
 import $ from 'jquery';
 import 'slick-carousel';
 import 'slick-carousel/slick/slick.css';
+
+const refs = {
+    moreInfoContainer: document.querySelector('.js-more-info'),
+    moreInfoItem: document.querySelector('.js-more-info__item'),
+    daysContainer: document.querySelector('.js-five-days'),
+};
+
+$('.input-list').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    nextArrow: document.querySelector('.arrow__prev'),
+    prevArrow: document.querySelector('.arrow__next'),
+    appendDots: false,
+});
