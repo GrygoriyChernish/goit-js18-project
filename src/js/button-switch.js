b4147db4bc2f8165036816131e81720f01cf1f08
Serrhii07 <serge070989@gmail.com>
2020-05-21 22:39:34 +0300

    fix(button-switch.js):hide quote/day for 5 days

--- src/js/button-switch.js
+++ src/js/button-switch.js
@@ -2,6 +2,7 @@ const refs = {
     btnWrap: document.querySelector('.btn__wrap'),
     citySection: document.querySelector('.city__section'),
     degree: document.querySelector('.degree'),
+    wrapDateQuote: document.querySelector('.wrap'),
 };
 
 refs.btnWrap.addEventListener('click', onBtnClick);
@@ -10,9 +11,11 @@ function onBtnClick(event) {
     if (event.target.nodeName === 'BUTTON') {
         if (event.target.classList.contains('js-days')) {
             refs.degree.classList.add('is-hidden');
+            refs.wrapDateQuote.classList.add('is-hidden');
             refs.citySection.classList.remove('is-hidden');
         } else {
             refs.degree.classList.remove('is-hidden');
+            refs.wrapDateQuote.classList.remove('is-hidden');
             refs.citySection.classList.add('is-hidden');
         }
     }
