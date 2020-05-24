87dd2ee9d3815e16d994ade3c75f56e8ce07ac48
Dmytriy Kravhenko <andreich1@bigmir.net>
2020-05-24 09:12:20 +0300

    added location sibmit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import './js/add-cities-list';
 import showCurrentData from './js/show-current-data';
 import onBtnClick from './js/button-switch';
 import slickSkroll from './js/slick-skroll';
+import getCurrentPosition from './js/geolocation';
+console.log(dataGeoLocation);
 
 onBtnClick();
 import $ from 'jquery';
@@ -12,7 +14,7 @@ import 'slick-carousel/slick/slick.css';
 const refs = {
     moreInfoContainer: document.querySelector('.js-more-info'),
     moreInfoItem: document.querySelector('.js-more-info__item'),
-    daysContainer: document.querySelector('.js-five-days'),
+    daysContainer: document.querySelector('.js-five-days')
 };
 
 $('.js-input-list').slick({
@@ -25,5 +27,5 @@ $('.js-input-list').slick({
     variableWidth: true,
     nextArrow: document.querySelector('.arrow__prev'),
     prevArrow: document.querySelector('.arrow__next'),
-    appendDots: false,
+    appendDots: false
 });
