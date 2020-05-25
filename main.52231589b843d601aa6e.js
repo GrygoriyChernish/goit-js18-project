4e6e193875adbaf34c0447e00966fd5c3741f91f
Denis <d-h11@ukr.net>
2020-05-25 20:25:02 +0300

    Updates

--- main.52231589b843d601aa6e.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{QfWi:function(n,e,t){"use strict";t.r(e);t("u0UJ");var u=document.querySelector("#time"),o=(document.querySelector("#mounth"),function(n){return(parseInt(n,10)<10?"0":"")+n});!function n(){var e=new Date,t=e.getHours(),s=e.getMinutes(),i=e.getSeconds();t=t%24||24,u.innerHTML=o(t)+"<span>:</span>"+o(s)+"<span>:</span>"+o(i),setTimeout(n,1e3)}()},u0UJ:function(n,e,t){}},[["QfWi",1]]]);
-//# sourceMappingURL=main.52231589b843d601aa6e.js.map
