354d1c6636c504a4ff47c12c9043e3252ec8b8db
Dmytriy Kravhenko <andreich1@bigmir.net>
2020-05-25 11:54:08 +0300

    chenge location.js and litlle chenge date-time.js files

--- src/js/geolocation.js
+++ src/js/geolocation.js
@@ -1,5 +1,8 @@
+import { fetchBackgroundImage } from './apiService-bg';
+import { getRandomNumber, setBackgroundImage, clearHtml, createHtml, tranformData } from './date-time';
+
 const options = {
-    timeout: 5000,
+    timeout: 5000
 };
 
 function success(position) {
@@ -13,14 +16,35 @@ function success(position) {
     const queryApi = () => {
         const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
 
-        return fetch(url)
-            .then(response => response.json())
-            .then(data => data)
-            .catch(err => console.warn(err));
+        return fetch(url).then((response) => response.json()).then((data) => data).catch((err) => console.warn(err));
     };
 
-    queryApi().then(data => {
-        console.log(data);
+    queryApi().then((data) => {
+        function updateWeatherResult() {
+            clearHtml();
+            queryApi()
+                .then((data) => {
+                    const days = tranformData(data);
+                    createHtml(days);
+                })
+                .catch((err) => {
+                    console.error('Error: ', err);
+                    alert(`По запросу "${appState.currentCity}" найдено 0 местоположений`);
+                });
+        }
+
+        tranformData(data);
+
+        clearHtml();
+
+        getRandomNumber();
+
+        fetchBackgroundImage(data.weather[0].main)
+            .then(({ hits }) => {
+                setBackgroundImage(hits[getRandomNumber()].largeImageURL);
+            })
+            .catch((error) => console.log(error));
+        updateWeatherResult();
     });
 }
 
