dbdbdd7b7aa48cf7c9d10b2f32f58cc123d0ddaa
Denis <d-h11@ukr.net>
2020-05-28 19:12:51 +0300

    (slick-scroll.js):delete

--- src/index.js
+++ src/index.js
@@ -5,6 +5,22 @@ import './scss/styles.scss';
 import showCurrentData from './js/show-current-data';
 
 // Добавление в список избранных городов
+import $ from 'jquery';
+import 'slick-carousel';
+import 'slick-carousel/slick/slick.css';
+
+$('.js-input-list').slick({
+    dots: true,
+    infinite: true,
+    autoplay: false,
+    speed: 500,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    nextArrow: document.querySelector('.arrow__prev'),
+    prevArrow: document.querySelector('.arrow__next'),
+    appendDots: false,
+});
 import './js/add-cities-list';
 
 // Кнопки "Today" и "5 days"
@@ -18,3 +34,38 @@ import './js/date-time';
 
 // Запрос на 5 дней
 import './js/5days';
+// $('.days').slick({
+//     dots: true,
+//     infinite: false,
+//     speed: 300,
+//     slidesToShow: 4,
+//     slidesToScroll: 4,
+//     responsive: [
+//         {
+//             breakpoint: 1024,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 3,
+//                 infinite: true,
+//                 dots: true,
+//             },
+//         },
+//         {
+//             breakpoint: 600,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 2,
+//             },
+//         },
+//         {
+//             breakpoint: 480,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1,
+//             },
+//         },
+//         // You can unslick at a given breakpoint now by adding:
+//         // settings: "unslick"
+//         // instead of a settings object
+//     ],
+// });
