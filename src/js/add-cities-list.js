5c709ea23f52c54bf21701a6e8c6e8a99bb4d2e3
ekaterina8aleksa <ekaterina.palad87@gmail.com>
2020-05-28 18:54:50 +0300

    fix(add list): delete, btn-hidding in process

--- src/js/add-cities-list.js
+++ src/js/add-cities-list.js
@@ -4,6 +4,8 @@ const refs = {
     citiesList: document.querySelector('.js-input-list'),
     starInput: document.querySelector('.js-star'),
     cityInput: document.querySelector('.js-form__input'),
+    prevBtn: document.querySelector('.arrow__prev'),
+    nextBtn: document.querySelector('.arrow__next'),
 };
 let favouriteCities = localStorage.getItem('cities')
     ? JSON.parse(localStorage.getItem('cities'))
@@ -11,13 +13,17 @@ let favouriteCities = localStorage.getItem('cities')
 localStorage.setItem('cities', JSON.stringify(favouriteCities));
 //Listiners
 refs.starInput.addEventListener('click', addToFavoriteCities);
-//refs.citiesList.addEventListener('click', onCloseIconClick);
+refs.citiesList.addEventListener('click', onCloseIconClick);
 refs.cityInput.addEventListener('click', Active);
 //Render List
 function renderCitiesList(cities) {
     if (refs.citiesList.firstChild) {
         refs.citiesList.innerHTML = '';
+        refs.prevBtn.classList.add('is-hidden');
+        refs.nextBtn.classList.add('is-hidden');
     } else {
+        refs.prevBtn.classList.remove('is-hidden');
+        refs.nextBtn.classList.remove('is-hidden');
         const markup = cities.reduce((acc, city) => acc + cityItem(city), '');
         return refs.citiesList.insertAdjacentHTML('beforeend', markup);
     }
