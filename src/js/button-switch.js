d17b573b1ac945d2cc4f3b59ffc7577c174fc592
Хворостяный Денис Николаевич <d-h11@ukr.net>
2020-05-28 14:36:49 +0300

    fix(all): finish fix all styles

--- src/js/button-switch.js
+++ src/js/button-switch.js
@@ -1,4 +1,5 @@
 const refs = {
+    mainBackground: document.querySelector('.background'),
     btnWrap: document.querySelector('.btn__wrap'),
     citySection: document.querySelector('.city__section'),
     degree: document.querySelector('.js-degree'),
@@ -15,6 +16,8 @@ function onBtnClick(event) {
         if (event.target.classList.contains('js-days')) {
             addClassList(refs.degree, 'is-hidden');
             addClassList(refs.wrapDateQuote, 'is-hidden');
+            addClassList(refs.btnWrap, 'btn__wrap-fix');
+            addClassList(refs.mainBackground, 'background-fix');
             removeClassList(refs.citySection, 'is-hidden');
 
             refs.fiveDaysBtn.disabled = true;
@@ -22,6 +25,8 @@ function onBtnClick(event) {
         } else {
             removeClassList(refs.degree, 'is-hidden');
             removeClassList(refs.wrapDateQuote, 'is-hidden');
+            removeClassList(refs.btnWrap, 'btn__wrap-fix');
+            removeClassList(refs.mainBackground, 'background-fix');
             addClassList(refs.citySection, 'is-hidden');
             refs.daysContainer.innerHTML = '';
 
